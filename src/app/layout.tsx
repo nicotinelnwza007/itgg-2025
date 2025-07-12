eebc03a518cc04b360c811702bb44def38c18a07
Nicotinelnwza007 <kaito.katang@gmail.com>
2025-07-12 15:14:03 +0700

    Setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from "next";
+import { Geist, Noto_Sans_Thai } from "next/font/google";
+import "./globals.css";
+
+const geist = Geist({
+  variable: "--font-geist",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+});
+
+const notoSansThai = Noto_Sans_Thai({
+  variable: "--font-noto-sans-thai",
+  subsets: ["thai"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+  display: "swap",
+});
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+
+export const metadata: Metadata = {
+  title: "itgg-2025",
+  description: "itgg-2025",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geist.variable} ${notoSansThai.variable}  antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center font-bold text-9xl min-h-screen">
+      it gg 2025 jaaaaaaaaa
+    </div>
+  );
+}
