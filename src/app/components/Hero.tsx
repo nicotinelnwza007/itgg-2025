75c24a35a05a098ac77449a522125fa419d88fd2
strdoffin <undefu5624@gmail.com>
2025-07-14 09:22:54 +0700

    change to tournament table

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -40,7 +40,7 @@ const Hero = () => {
 
           <Link href="/know" className="w-full sm:w-auto">
             <button className="w-full sm:w-auto cursor-pointer inline-flex items-center justify-center gap-2 rounded-md border border-amber-700 text-amber-700 bg-white hover:bg-amber-700 hover:text-white shadow-md hover:scale-105 active:scale-95 disabled:opacity-50 disabled:pointer-events-none outline-none focus:ring-2 focus:ring-blue-500 transition-all duration-200 ease-in-out h-11 px-6 py-2 text-lg sm:text-xl font-bold">
-              เรียนรู้เพิ่มเติม
+              ตารางการแข่งขัน
             </button>
           </Link>
         </div>
