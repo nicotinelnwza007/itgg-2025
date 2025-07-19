0a5a0b669e0cc2c0f3d83084bc25039dc3564773
nicotinelnwza007 <160930169+nicotinelnwza007@users.noreply.github.com>
2025-07-19 11:44:21 +0700

    hydration error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import Image from "next/image";
-import Countdown from "./components/countdown";
+"use client";
+
+import dynamic from "next/dynamic";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Benefits from "./components/Benefits";
@@ -8,21 +9,26 @@ import Travelling from "./components/Travelling";
 import FAQs from "./components/FAQs";
 import ImageSlider from "./components/AutoImageSlide";
 import SweetScoreboard from "./components/Scoreboard";
+
+const Countdown = dynamic(() => import("./components/countdown"), {
+  ssr: false,
+});
+
 export default function Home() {
-  // Date.UTC(YEAR, MONTH_INDEX, DAY, HOUR, MINUTE, SECOND)
-  // didnt convert to gmt +7 yet this is time gmt +0
-  const target = new Date(Date.UTC(2025, 6, 21, 17, 0, 0));
+  // This time is in UTC (2025-07-21 17:00:00 UTC)
+  const target = new Date(Date.UTC(2025, 6, 21, 17, 0, 0)); // GMT+0
+
   return (
     <div className="flex flex-col justify-center items-center">
       <Countdown targetTime={target} />
       <Hero />
       <About />
-      <SweetScoreboard/>
+      <SweetScoreboard />
       <Benefits />
       <Agenda />
       <Travelling />
       <FAQs />
-      <ImageSlider/>
+      <ImageSlider />
     </div>
   );
 }
