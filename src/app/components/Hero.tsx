04b8df09444fe710310253e73d03af354c836e70
nicotinelnwza007 <160930169+nicotinelnwza007@users.noreply.github.com>
2025-07-21 02:24:31 +0700

    Layout

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 import SendAnswer from "./SendAnswer";
 const Hero = () => {
   return (
-    <section className="flex justify-center items-center w-full min-h-screen mt-8  px-4  sm:px-6 lg:px-8 py-10 sm:py-14 md:py-20">
+    <section className="flex justify-center items-center w-full min-h-screen mt-8 px-4 sm:px-6 lg:px-8 py-10 sm:py-14 md:py-20 overflow-x-hidden">
+
       <div className="max-w-5xl mx-auto flex flex-col items-center space-y-2 sm:space-y-5 md:space-y-2">
         {/* Logo Section */}
         <div className="w-full flex justify-center">
@@ -18,9 +19,11 @@ const Hero = () => {
 
         {/* Text Section */}
         <div className="text-center  text-white">
-          <h1 className="font-agbalumo text-6xl sm:text-5xl md:text-8xl font-bold mb-2 text-glow">
-            ITGG 2025
-          </h1>
+        <h1 className="font-agbalumo text-6xl sm:text-5xl md:text-8xl font-bold mb-2 text-glow break-words max-w-full text-center">
+          ITGG 2025
+        </h1>
+
+
             <p className="text-base sm:text-lg md:text-2xl leading-relaxed max-w-5xlxl mx-auto mt-12">
               InfoTech Tournament GateGame 2025 หรือ ITGG 2025 : มหกรรมการแข่งขันที่ยิ่งใหญ่ที่สุดในไอทีลาดกระบัง เตรียมตัวให้พร้อม! กับเวทีโชว์ศักยภาพ ทักษะด้านกีฬา และ E-sports
             </p>
