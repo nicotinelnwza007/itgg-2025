e62e19058b5287beb6719d22f4c15c4b4e31d5e7
Gxnp <guynopan@gmail.com>
2025-07-31 01:37:15 +0700

    edit api

--- src/app/components/SendAnswer.tsx
+++ src/app/components/SendAnswer.tsx
@@ -72,7 +72,7 @@ export default function SendAnswer() {
 		setMessage('')
 
 		try {
-			const res = await fetch('/api/submit_answer', {
+			const res = await fetch('/api/check_answer', {
 				method: 'POST',
 				headers: { 'Content-Type': 'application/json' },
 				body: JSON.stringify({ id: dailyQuest.id, answer }),
